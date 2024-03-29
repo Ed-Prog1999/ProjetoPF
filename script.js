ee5bb35db2891cdd0ea35bb4f3f5c7854401bf4b
Edson Santana <edson20sann@gmail.com>
2024-03-29 12:23:43 -0300

    atualizãção do codigo de leitura e primeira pergunta

--- script.js
+++ script.js
@@ -1,42 +1,79 @@
-const fs = require('fs').promises;
-
+// usei como base arquvi de leitura disponibilizado para aula
 const lerArquivoCsv = async (caminhoArquivo) => {
   try {
-    const data = await fs.readFile(caminhoArquivo, 'utf8');
-
-    // Dividindo as linhascopia considerando \r?\n como separador
-    const linhas = Object.freeze(data.split(/\r?\n/));
-    const linhascopia = [...linhas];
-
-    // Removendo o cabeçalho e dividindo as colunas
-    const [cabecalho, ...linhasDados] = linhascopia;
-    const camposCabecalho = cabecalho.split(',').map(item => item.replace(/"/g, ''));
-
-    // Mapear as linhascopia de dados
-    const dados = linhasDados.map(linha => {
-      const campos = linha.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/); // Divide considerando vírgulas fora de aspas
-      return campos.reduce((obj, campo, index) => {
-        obj[camposCabecalho[index]] = campo.replace(/"/g, ''); // Remove as aspas dos valores
-        return obj;
-      }, {});
-    });
-
-    return dados;
+      const response = await fetch(`${caminhoArquivo}`);
+      if (!response.ok) {
+          throw new Error('Falha ao carregar o arquivo');
+      }
+      const csvContent = await response.text();
+      return csvContent;
   } catch (err) {
-    throw err;
+      throw err;
   }
 };
 
+const parseCSV = (csv) => {
+  //separação de campos por ',' fora de aspas , usando o split para a formatação em quebra de linha 
+  const lines = csv.split(/\r?\n/);
+  const [cabecalho, ...linhasDados] = lines;
+
+  const camposCabecalho = cabecalho.split(',');
+  //copias de dados para uso do replaace
+  const copiaCampoCabecalho = [...camposCabecalho].map(item => item.replace(/"/g, ''));
+
+  const dados = linhasDados.map(linha => {
+      // split cm parametro para ignorar virgulas entre aspas
+      const campos = linha.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+      return campos.reduce((obj, campo, index) => {
+          obj[copiaCampoCabecalho[index]] = campo.replace(/"/g, '');
+          return obj;
+      }, {});
+  });
+
+  return dados;
+}
+
 // Exemplo de uso com async/await
 const caminhoArquivoAtletas = 'atletas.csv';
-//const caminhoArquivoNoc = 'noc.csv';
 
 (async () => {
   try {
-    const resultado = await lerArquivoCsv(caminhoArquivoAtletas);
-    console.log('Dados lidos:', resultado);
+    const conteudoArquivo = await lerArquivoCsv(caminhoArquivoAtletas);
+    const resultado = parseCSV(conteudoArquivo)
+
+    //console.log('Dados lidos:', resultado);
+    
+    // Questão 1 , acerca de idades , o usuários escolhe entre o mais novo ou mais velho participante ou participantes registrados
+    // constantes para definir maior ou menor idade 
+    const idades = (idadesMaiorMenor) => (lista = resultado) => {
+      const compararIdades = (a, b) => {
+          if (idadesMaiorMenor === 'maior') {
+              return b.Age - a.Age; // Ordenar por maior idade
+          } else if (idadesMaiorMenor === 'menor') {
+              return a.Age - b.Age; // Ordenar por menor idade
+          }
+      };
+      // uso do sort em uma copia da lista para garantir a imutabilidade da lista original
+      const copiaLista = [...lista];
+      const ordenaListaIdades = copiaLista.sort(compararIdades).map(x => `${x.Name}, ${x.Age} anos, atleta do pais( time ): ${x.Team}, na temporada ${x.Season} de ${x.Year} , competiu pelo esport de ${x.Sport}`);
+      return ordenaListaIdades[0]
+    }
+   // Obtém os elementos HTML
+  const btnMaisNovo = document.getElementById('btnMaisNovo');
+  const btnMaisVelho = document.getElementById('btnMaisVelho');
+  const resultadoLabel = document.getElementById('resultado');
+    
+  // Adiciona event listeners aos botões
+  btnMaisNovo.addEventListener('click', () => {
+  // Exibe o resultado da função idades com a menor idade no elemento <label>
+      resultadoLabel.textContent = idades('menor')();
+  });
+  btnMaisVelho.addEventListener('click', () => {
+  // Exibe o resultado da função idades com a maior idade no elemento <label>
+      resultadoLabel.textContent = idades('maior')();
+  });
+
   } catch (err) {
     console.error('Erro ao ler o arquivo:', err);
   }
 })();
-
