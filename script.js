ff5df8e4673d13191bd10af141a28f3928d4ec0c
Edson Santana <edson20sann@gmail.com>
2024-03-31 10:21:21 -0300

    adição questão 2

--- script.js
+++ script.js
@@ -45,18 +45,20 @@ const caminhoArquivoAtletas = 'atletas.csv';
     
     // Questão 1 , acerca de idades , o usuários escolhe entre o mais novo ou mais velho participante ou participantes registrados
     // constantes para definir maior ou menor idade 
-    const idades = (idadesMaiorMenor) => (lista = resultado) => {
-      const compararIdades = (a, b) => {
-          if (idadesMaiorMenor === 'maior') {
-              return b.Age - a.Age; // Ordenar por maior idade
-          } else if (idadesMaiorMenor === 'menor') {
-              return a.Age - b.Age; // Ordenar por menor idade
-          }
-      };
+ 
+      const ordenaListaPorCampo =  (idadeMaisMenos) => (lista = resultado) => {
+        const resolverNA = lista.filter( x => x.Age !== 'NA')
+        const compararIdades = (a, b) => {
+            if (idadeMaisMenos === 'maior') {
+                return b.Age - a.Age; // Ordenar por maior idade
+            } else if ( idadeMaisMenos === 'menor') {
+                return a.Age - b.Age; // Ordenar por menor idade
+            }
+        };
       // uso do sort em uma copia da lista para garantir a imutabilidade da lista original
-      const copiaLista = [...lista];
-      const ordenaListaIdades = copiaLista.sort(compararIdades).map(x => `${x.Name}, ${x.Age} anos, atleta do pais( time ): ${x.Team}, na temporada ${x.Season} de ${x.Year} , competiu pelo esport de ${x.Sport}`);
-      return ordenaListaIdades[0]
+      const copiaLista = [...resolverNA];
+      const ordenaListaIdades = copiaLista.sort(compararIdades).map(x => `${x.Name},com inacreditaveis ${x.Age} anos, atleta do pais( time ): ${x.Team}, na temporada ${x.Season} de ${x.Year} , competiu pelo esport de ${x.Sport}`);
+      return ordenaListaIdades[0] // retorna 58 , 0 maior em ordem e 12 , o menor 
     }
    // Obtém os elementos HTML
   const btnMaisNovo = document.getElementById('btnMaisNovo');
@@ -66,11 +68,48 @@ const caminhoArquivoAtletas = 'atletas.csv';
   // Adiciona event listeners aos botões
   btnMaisNovo.addEventListener('click', () => {
   // Exibe o resultado da função idades com a menor idade no elemento <label>
-      resultadoLabel.textContent = idades('menor')();
+      resultadoLabel.innerHTML = ordenaListaPorCampo('menor')();
   });
   btnMaisVelho.addEventListener('click', () => {
   // Exibe o resultado da função idades com a maior idade no elemento <label>
-      resultadoLabel.textContent = idades('maior')();
+      resultadoLabel.innerHTML = ordenaListaPorCampo('maior')();
+  });
+
+  //Questão2 - sobre edições de  olimpiadas
+  // cria uma lista de registro com campo e ocorrencias de determinado campo
+  //botão1 - primeiro ano de olimpiadas modernas
+  const contarOcorrenciasPorId = resultado.reduce((contador, controle) => {
+    const { Year } = controle;
+    if (!isNaN(parseInt( Year ))) {
+      contador[ Year ] = (contador[ Year ] || 0) + 1;
+    }
+    return contador;
+  }, {});
+  
+  const listaOcorrencias = Object.entries(contarOcorrenciasPorId).map(([ Year, ocorrencias]) => ({
+    Year: parseInt(Year),
+    ocorrencias
+  }));
+  
+  const primeiraEdicaOlimpModerna = listaOcorrencias[0].Year
+  // adição de eventos ao html
+  const btprimeiraEdicao = document.getElementById('primeiraEdicao');
+  const btnumeroEdicoes = document.getElementById('numeroEdicoes');
+  const resultadoLabelQ2 = document.getElementById('resultadoQ2');
+    
+  // Adiciona event listeners aos botões
+  btprimeiraEdicao.addEventListener('click', () => {
+  // const com resultados 
+      resultadoLabelQ2.innerHTML = `A primeira edição das novas olímpiadas ocorreu no ano de: ${primeiraEdicaOlimpModerna}`
+  });
+  
+  //console.log(primeiraEdicaOlimpModerna)
+
+  //botão2 - numero de ediçoes desde a primeira , até 2016
+  const numeroEdicoes = listaOcorrencias.map( x => x.Year).reduce((acc) => acc + 1, 0)
+  
+  btnumeroEdicoes.addEventListener('click', () => {
+        resultadoLabelQ2.innerHTML = `Desde a ocorrência da primeira olimpiada moderna , <br> ja ocorreram ${numeroEdicoes} edições (segundo dados de arquivo) , entre temporadas de verão e inverno.<br>Na história apenas duas vezes o evento foi cancelado.   Motivo ? 1a e 2a guerra mundial`
   });
 
   } catch (err) {
