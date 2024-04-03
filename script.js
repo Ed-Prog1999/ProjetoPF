935a9478798745e4a3d60fb95fce44bd13cbbfa9
Edson Santana <edson20sann@gmail.com>
2024-04-02 22:36:17 -0300

    atualização de abordagem ao projeto

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const caminhoArquivoAtletas = 'atletas.csv';
     const conteudoArquivo = await lerArquivoCsv(caminhoArquivoAtletas);
     //Object.Freeze para congelar e garantir a imutabilidade dos dados obtidos através do arquivo fornecido
     const resultado = Object.freeze(parseCSV(conteudoArquivo))
-
+    const copiaDadosResultado = [...resultado]
     //console.log('Dados lidos:', resultado);
 
     //funçõa currificada para contar ocorrencias de determinado campo e agrupar em objetos , vai facilitar e possibilitar a contagem de objetos
@@ -58,7 +58,7 @@ const caminhoArquivoAtletas = 'atletas.csv';
         return resultado;
     };
     
-    //essa outra função trabalha em conjunto com a função acima, ela pega o unico ojeto gerado pela função acima e os formata
+    //essa outra função trabalha em conjunto com a função acima, ela pega o unico objeto gerado pela função acima e os formata
     //em uma lista de objetos , onde cada objeto é o valor de um campo e quantas ocorrencias cada um teve
     const criaListaObjetos = (lista) => (campo) => {
       return Object.entries(lista).map(([chave, ocorrencias]) => ({
@@ -67,8 +67,7 @@ const caminhoArquivoAtletas = 'atletas.csv';
       }));
     };
   
-    
-    // Questão 1 , acerca de idades , o usuários escolhe entre o mais novo ou mais velho participante ou participantes registrados
+    // Questão 1 , acerca de idades dos atletas , o usuários escolhe entre o mais novo ou mais velho participante ou participantes registrados
     // constantes para definir maior ou menor idade 
     const OrdenaListaPorIdade =  (idadeMaisMenos) => (lista = resultado) => {
         const resolverNA = lista.filter( x => x.Age !== 'NA')
@@ -84,13 +83,13 @@ const caminhoArquivoAtletas = 'atletas.csv';
       // uso do sort em uma copia da lista para garantir a imutabilidade da lista original
       const copiaLista = [...resolverNA];
       const ordenaListaIdades = copiaLista.sort(compararIdades).map(x => `${x.Name} , com inacreditaveis ${x.Age} anos , <br>
-         atleta do pais( time ): ${x.Team} , competiu na temporada ${x.Season} de ${x.Year} , quando tinha tal idade, pelo esport de ${x.Sport}.`);
+         atleta do pais( time ): ${x.Team} ,quando tinha tal idade, competiu na temporada ${x.Season} de ${x.Year} , pelo esport de ${x.Sport}.`);
       return ordenaListaIdades[0] 
     }
    // Obtém os elementos HTML
   const btMaisNovo = document.getElementById('btnMaisNovo');
   const btMaisVelho = document.getElementById('btnMaisVelho');
-  const resultadoLabel = document.getElementById('resultado');
+  const resultadoLabel = document.getElementById('resultadoQ1');
     
   // Adiciona event listeners aos botões , menor idade
   btMaisNovo.addEventListener('click', () => {
@@ -102,39 +101,7 @@ const caminhoArquivoAtletas = 'atletas.csv';
       resultadoLabel.innerHTML = OrdenaListaPorIdade('maior')();
   });
 
-  //Questão2 - sobre edições de  olimpiadas
-  //Reuso da função criaObjetoComValoresDeCampoOcorrencia com um dos parametros sendo o ano, para conseguir contar o numero de edições 
-  // e ver quando ocorreu a primeira olimpiada
-  
-  
-  const primeiraEdicaOlimpModerna = criaListaObjetos(criaObjetoComValoresDeCampoOcorrencia(resultado)('Year'))('Year')
-  const ultimaEdicaoOlimpicaCatalogada = primeiraEdicaOlimpModerna[ primeiraEdicaOlimpModerna.length - 1].Year
-
-  // adição de eventos ao html
-  //botão1 - primeiro ano de olimpiadas modernas
-  const btprimeiraEdicao = document.getElementById('primeiraEdicao');
-  const btnumeroEdicoes = document.getElementById('numeroEdicoes');
-  const resultadoLabelQ2 = document.getElementById('resultadoQ2');
-    
-  // Adiciona event listeners aos botões
-  btprimeiraEdicao.addEventListener('click', () => {
-  // const com resultados 
-      resultadoLabelQ2.innerHTML = `A primeira edição das novas olímpiadas ocorreu no ano de: ${primeiraEdicaOlimpModerna[0].Year}`
-  });
-  
-
-  //botão2 - numero de ediçoes desde a primeira , até 2016
-  //função usandoo o reduce para contar ,por ano, a qntdd de edições  
-  const numeroEdicoes = primeiraEdicaOlimpModerna.reduce((acc) => acc + 1, 0)
-
-  btnumeroEdicoes.addEventListener('click', () => {
-        resultadoLabelQ2.innerHTML = `Desde a ocorrência da primeira olimpiada moderna , 
-          <br> ja ocorreram ${numeroEdicoes} edições (segundo dados de arquivo) , entre temporadas de verão e inverno.
-          <br>Na história apenas duas vezes o evento foi cancelado.   Motivo ? 1a e 2a guerra mundial`
-  });
-
-
-  // Q3 - homens e mulheres nas olimpiadas
+  // Q2 - Atletas homens e Atles mulheres
   // reuso da função criaObjetoComValoresDeCampoOcorrencia agr usando como parametro atletas
   //novas constante para armazenar a lista filtrada por sexo
   const sexMasculino = resultado.filter(x => x.Sex == 'M')
@@ -146,76 +113,35 @@ const caminhoArquivoAtletas = 'atletas.csv';
   const totalAtletas = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia(resultado)('Name'))('Name').length
   
   const porcentagemReferenteaOutroValor = ( valor1 ) => ( valor2 ) => (valor1 / valor2) * 100
+  // usando length - 2 como indice do ultimo elemento , devido o length - 1 retorna a ultima linha do arquivo , que está vazia
+  const tempoDeEventoOlimpico = copiaDadosResultado.sort((a , b) => a.Year - b.Year)
 
-  //botões questão 3
+  //botões questão 2
   const btHomens = document.getElementById('numeroHomens');
   const btMulheres = document.getElementById('numeroMulheres');
   const btTotal = document.getElementById('numeroTotal');
-  const resultadoQ3Label = document.getElementById('resultadoQ3')
+  const resultadoQ2Label = document.getElementById('resultadoQ2')
     
   // Adiciona event listeners aos botões
   btHomens.addEventListener('click', () => {
   // Exibe o resultado da função idades com a menor idade no elemento <label>
-      resultadoQ3Label.innerHTML = `Desde a primeira edição das olimpiadas modernas, 
+      resultadoQ2Label.innerHTML = `Desde a primeira edição das olimpiadas modernas, 
         dentre todas até a ultima catalogada( 2016) um total de ${totalHomens} atletas homens participaram de olimpiadas.`
   });
   btMulheres.addEventListener('click', () => {
   // Exibe o resultado da função idades com a maior idade no elemento <label>
-    resultadoQ3Label.innerHTML = `As mulheres na história das olimpíadas , tambem tiveram  seu papel , 
+    resultadoQ2Label.innerHTML = `As mulheres na história das olimpíadas , tambem tiveram  seu papel , 
       foram ${totalMulheres} que ja participaram  desde sua primeira edição, 
-      um numero expressivo , ainda assim , distante do total de homens , 
+      um numero expressivo ,<br> ainda assim , distante do total de homens , 
       elas representam apenas ${(porcentagemReferenteaOutroValor(totalMulheres)(totalHomens)).toFixed(2)}% do total de atletas que ja participaram em edições.`
   });
   btTotal.addEventListener('click', () => {
   // Exibe o resultado da função idades com a maior idade no elemento <label>
-      resultadoQ3Label.innerHTML = `Em ${ ultimaEdicaoOlimpicaCatalogada - primeiraEdicaOlimpModerna[0].Year } anos de história moderna e jogos olimpicos ,
+      resultadoQ2Label.innerHTML = `Em ${ tempoDeEventoOlimpico[tempoDeEventoOlimpico.length - 2 ].Year - tempoDeEventoOlimpico[0].Year } anos de história moderna de jogos olimpícos ,
         um total de ${totalAtletas} atletas ja competiram no evento.`
   });
 
-  //questão4 - sobre temporadas
-  //const armazenando o numero de sports e quantidade de vezes q  foi citado
-    
-    //constantes armazenando as temporadas de inverno  e verão separadamente
-    const filtraTemporada = ( lista ) => ( temporada ) => lista.filter( x => x.Season == temporada)
-    const verao = filtraTemporada(resultado)('Summer')
-    const inverno = filtraTemporada(resultado)('Winter')
 
-    // temporada verao
-    const ano1TemporadaVerao = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia (verao)('Year') )('Year')
-    const numeroDeAtletasQParticiparamVerao = criaListaObjetos ( criaObjetoComValoresDeCampoOcorrencia(resultado.filter(x => x.Year == ano1TemporadaVerao[0].Year))('Name') )('Name').length
-    const quantidadeSports = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia(verao)('Sport'))('Sport').length
-    const quantidadeModalidades = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia(verao)('Event'))('Event').length
-
-    
-    //temporada inverno
-    const ano1TemporadaInverno = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia(inverno)('Year') )('Year')
-    const numeroDeAtletasQParticiparamInverno = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia(resultado.filter( x => x.Year == ano1TemporadaInverno[0].Year))('Name') )('Name').length
-    const quantidadeSportsInverno = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia(inverno)('Sport') )('Sport').length
-    const quantidadeModalidasInverno = criaListaObjetos( criaObjetoComValoresDeCampoOcorrencia(inverno)('Event'))('Event').length
-
-    //botoes
-    const btVerao = document.getElementById('verao')
-    const btInverno = document.getElementById('inverno')
-    const resultadoQ4Label = document.getElementById('resultadoQ4')
-
-    //acionamente e exibição de botões/resultados
-    //bt verão
-    //const contendo registro cm dados da primeria olimpiada
-    const dadosPrimeiraOlimpiada = resultado.filter( x => x.Year == primeiraEdicaOlimpModerna[0].Year )
-    const dadosPrimeiraOlimpiadaInverno = resultado.filter( x => x.Season == 'Winter'  &&  x.Year == ano1TemporadaInverno[0].Year)
-
-    btVerao.addEventListener('click', () => {
-        resultadoQ4Label.innerHTML = `Inicialmente os jogos olímpicos eram apenas em sua temporada de verão , <br>
-          foi em ${dadosPrimeiraOlimpiada[0].Year}, na cidade de ${dadosPrimeiraOlimpiada[0].City}, no país da ${dadosPrimeiraOlimpiada[0].Team},
-          com um  total de ${numeroDeAtletasQParticiparamVerao} atletas , competindo em ${quantidadeSports} esportes e ${quantidadeModalidades} modalidades.`
-    })
-
-    //bt inverno
-    btInverno.addEventListener('click', () =>{
-        resultadoQ4Label.innerHTML = `A primeira competição de carater mundial , ocorreu em ${ano1TemporadaInverno[0].Year}, sediada em ${dadosPrimeiraOlimpiadaInverno[0].City} ,
-          ${dadosPrimeiraOlimpiadaInverno[6].Team}, com ${numeroDeAtletasQParticiparamInverno} competidores , disputando entre ${quantidadeSportsInverno} esportes 
-          e ${quantidadeModalidasInverno} modalidades.`
-    })
 
   } catch (err) {
     console.error('Erro ao ler o arquivo:', err);
